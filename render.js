36b5cc00057e8b9b74c5bf4743911abe070cd715
vladislavklimov <vvklimov95@yandex.ru>
2023-07-16 20:02:30 +0200

    Add dist folder for build

--- render.js
+++ render.js
@@ -4,29 +4,44 @@ const Handlebars = require('handlebars')
 
 const templateFilename = './src/template.hbs'
 const dataFilename = './src/data.yaml'
-const outputFilename = './index.html'
+const cssPath = './css'
+const outputDirectory = './dist'
+const outputFilename = `${outputDirectory}/index.html`
+const outputPdfFileName = `${outputDirectory}/resume.pdf`
 
 const contains = (list, element) => list.filter(x => x === element).length > 0
 
-const render = (templateFilename, dataFilename, outputFilename) => {
+const prepearingBuildDirectory = (outputDirectory) => {
+    const isDirExist = fs.existsSync(outputDirectory)
+    if (isDirExist) {
+        console.log('Removed previos build repository')
+        fs.rmSync(outputDirectory, {recursive: true, force: true})
+    }
+
+    console.log('Created repository for new build')
+    fs.mkdirSync(outputDirectory)
+}
+
+const render = (templateFilename, dataFilename, outputFilename, outputDirectory) => {
+    prepearingBuildDirectory(outputDirectory)
     const template = Handlebars.compile(fs.readFileSync(templateFilename, 'utf8'))
     const data = jsyaml.load(fs.readFileSync(dataFilename, 'utf8'))
         
     console.log('Rendering template')
     const html = template(data) 
     console.log(html)
-    
-    fs.writeFileSync(outputFilename, html)
+    fs.appendFileSync(outputFilename, html)
     console.log(`${html.length} bytes written`)    
+    fs.cpSync(cssPath, `${outputDirectory}/css`, {recursive: true})
 }
 
-render(templateFilename, dataFilename, outputFilename)
+render(templateFilename, dataFilename, outputFilename, outputDirectory)
 
 if (contains(process.argv, '--pdf')) {
     const pdf = require('html-pdf')
     const options = {}
     const html = fs.readFileSync(outputFilename, 'utf8')
-    pdf.create(html, options).toFile('./resume.pdf', function (err, res) {
+    pdf.create(html, options).toFile(outputPdfFileName, function (err, res) {
         if (err) {
             console.log(err)
         }
@@ -39,7 +54,7 @@ if (contains(process.argv, '--watch')) {
     fs.watch('./src', (event, filename) => {
         console.log(`${event}: ${filename}`)
         try {
-            render(templateFilename, dataFilename, outputFilename)    
+            render(templateFilename, dataFilename, outputFilename, outputDirectory)    
         } catch (error) {
             console.log(`Rendering failed: ${error}`)
         }        
